23b21a2d985cefa4378a87c377d3d071b1df196c
Gary <garrett.rogers002@gmail.com>
2026-02-09 13:28:29 -0500

    change fields in Book to private, and final

--- app/src/main/java/org/yourcompany/library/models/Book.java
+++ app/src/main/java/org/yourcompany/library/models/Book.java
@@ -1,22 +1,22 @@
 package org.yourcompany.library.models;
 
 public class Book {
-    String title;
-    String author;
-    boolean isCheckedOut = false;
+    private final String title;
+    private final String author;
+    private boolean isCheckedOut = false;
 
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
     public String getTitle() {
-        return this.title;
+        return title;
     }
     public String getAuthor() {
-        return this.author;
+        return author;
     }
     public boolean getIsCheckedOut() {
-        return this.isCheckedOut;
+        return isCheckedOut;
     }
     public void checkOut() {
         this.isCheckedOut = true;
